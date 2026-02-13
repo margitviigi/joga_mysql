01345dc21bf6d25c1a3bac7ad5161ceef51ced1d
margitviigi <margit.viigi@voco.ee>
2026-02-13 13:18:54 +0200

    Added handlebars template engine

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // Ensure environment variables are loaded before other modules
 require('dotenv').config();
 
+const hbs = require('express-handlebars');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}));
+
+
 const App = require('./utils/app')
 const app = new App(3025)
